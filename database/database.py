0a8895bfb3300a1f30c78ddbaf07a6b38998f57d
Vitor <vitortrindadeborher>
2026-05-21 19:08:31 -0300

    Fix postgresql url scheme for sqlalchemy

--- database/database.py
+++ database/database.py
@@ -6,7 +6,8 @@ import os
 
 DEFAULT_DATABASE_URL = "sqlite:///./sql_app.db"
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", DEFAULT_DATABASE_URL)
-
+if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
